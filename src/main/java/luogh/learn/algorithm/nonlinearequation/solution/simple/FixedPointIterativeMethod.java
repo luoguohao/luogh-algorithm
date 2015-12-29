f8a7f73dac84e3ce0a3f8dc89ebc12b3d0289b50
luoguohao <869000768@qq.com>
2015-12-29 16:28:30 +0800

    convert to maven project. and add the method to compute the solution of the Non-Linear Equation

---
+++ src/main/java/luogh/learn/algorithm/nonlinearequation/solution/simple/FixedPointIterativeMethod.java
@@ -0,0 +1,7 @@
+package luogh.learn.algorithm.nonlinearequation.solution.simple;
+
+/**
+ * Created by Kaola on 2015/12/29.
+ */
+public class FixedPointIterativeMethod {
+}
---
+++ src/main/java/luogh/learn/algorithm/utils/MathUtil.java
@@ -0,0 +1,10 @@
+package luogh.learn.algorithm.utils;
+
+/**
+ * Created by Kaola on 2015/12/29.
+ */
+public class MathUtil {
+	public static int sgn(double num){
+		return num>0?1:(num==0)?0:-1;
+	}
+}
